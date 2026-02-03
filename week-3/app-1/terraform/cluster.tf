711bc32fba8cc9ece1f19879949ab73fe3e16d49
twumasiackah@gmail.com <twumasiackah@1b3.cluster>
2026-02-03 15:02:39 +0100

    nur zwei nodes

--- week-3/app-1/terraform/cluster.tf
+++ week-3/app-1/terraform/cluster.tf
@@ -8,7 +8,7 @@ resource "stackit_ske_cluster" "ske_cluster_l3" {
       name               = "node-pool"
       machine_type       = "g1a.2d"
       minimum            = 2
-      maximum            = 3
+      maximum            = 2
       availability_zones = ["eu01-1", "eu01-2"]
       os_version_min     = "4459.2.1"
       os_name            = "flatcar"
---
+++ week-3/app-1/terraform/main.tf
@@ -0,0 +1,50 @@
+terraform {
+    required_providers {
+        stackit = {
+            source  = "stackitcloud/stackit"
+            version = "~> 0.79.0"
+        }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "~> 2.9.0"
+        }
+    }
+}
+
+# Authentication: Key flow (using path)
+provider "stackit" {
+  default_region           = "eu01"
+  service_account_key_path = var.service_account_key_path
+  private_key_path         = var.private_key_path
+}
+
+# Helm provider configuration
+# provider "helm" {
+#   kubernetes {
+#     config_path = "${path.module}/kubeconfig.yaml"
+#   }
+# }
+
+# locals {
+#   # Parse the raw YAML string into a Terraform object
+#   kubeconfig_data = yamldecode(stackit_ske_kubeconfig.ske_kubeconfig_l3.kube_config)
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     # Extract values from the decoded YAML structure
+#     host = local.kubeconfig_data.clusters[0].cluster.server
+
+#     client_certificate = base64decode(
+#       local.kubeconfig_data.users[0].user.client-certificate-data
+#     )
+    
+#     client_key = base64decode(
+#       local.kubeconfig_data.users[0].user.client-key-data
+#     )
+    
+#     cluster_ca_certificate = base64decode(
+#       local.kubeconfig_data.clusters[0].cluster.certificate-authority-data
+#     )
+#   }
+# }
