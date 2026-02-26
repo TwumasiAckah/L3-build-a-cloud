77c443fc679315974038cb3244342035d2d2fa5e
twumasiackah@gmail.com <twumasiackah@1b3.cluster>
2026-02-26 11:34:59 +0100

    update auditlogs & schema

--- week-5/react-cloud-ui/src/shared/schema.ts
+++ week-5/react-cloud-ui/src/shared/schema.ts
@@ -80,7 +80,7 @@ export type ServiceLog = {
   timestamp: string;
   level: "INFO" | "WARN" | "ERROR";
   message: string;
-  log_type?: "service";
+  log_type?: string;
   database_name?: string;
   event_type?: string;
   details?: Record<string, unknown>;
@@ -89,7 +89,7 @@ export type ServiceLog = {
 export type AuditLog = {
   timestamp: string;
   message: string;
-  log_type: "audit";
+  log_type: string;
   action?: string;
   resource_name?: string;
   user?: string;
